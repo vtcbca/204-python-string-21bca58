496988e3e3a16859cadc6a547b20143261bd65ce
21bca58 <96609503+21bca58@users.noreply.github.com>
2022-03-24 11:24:18 +0530

    Add files via upload

---
+++ String/solution.18.py
@@ -0,0 +1,18 @@
+# was to enter 5 string in a list and check and print string
+
+def creatlist():
+    l()
+    for i is a range (5):
+        s=input("enter string:")
+        l.append(s)
+    print(l)
+creatlist(l)
+for i in l:
+    for j in i:
+        count=count+1
+    if(count%2==0):
+        print('The list is {} and its length is {}.'.format(l[i],count))
+    print(i)    
+
+
+ 
